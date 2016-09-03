66165589420728793d986238ecb550595a0f673b
yama2211 <yama2211@ahk.jp>
2016-09-03 22:54:22 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+﻿function BAN() {
+	var text = document.getElementById("input").value;
+	document.getElementById("input").value = "";
+	document.getElementById("reason_text").innerHTML = text;
+
+	var dt = new Date();
+	var year = dt.getFullYear();
+	var month = dt.getMonth()+1;
+	var day = dt.getDate();
+	var hours = dt.getHours();
+	var minutes = dt.getMinutes();
+	var date = year + '/' + month + '/' + day +' '+ hours + ':' + minutes + ' ～';
+
+	document.getElementById("date").innerHTML = date;
+
+	if($(".result").css("display") == "none"){
+		$(".result").slideDown("fast");
+	}
+	window.scrollTo(0,0);
+}
+
+$(function(){
+	$("#input").focusin(function(e) {
+		if($(".result").css("display")!="none"){
+			$(".result").slideUp("fast");
+		}
+	});
+});
